064535ef16f1d267d90442f387aa435f67ad33a7
William Lowrimore <wlowrimore@gmail.com>
2023-01-16 15:06:05 -0600

    adds jump to resume button on about/home page

--- portfolio-code/src/components/profile/profile-image-card.component.jsx
+++ portfolio-code/src/components/profile/profile-image-card.component.jsx
@@ -1,14 +1,25 @@
 import { Fragment } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import {
   ImageCardContainer,
   AboutContainer,
   Title,
+  ButtonContainer,
 } from "./profile-image-card.styles";
 
 import AboutText from "../profile-about/about-text.component";
 import Selfie from "../../assets/selfPic.jpg";
+import Button from "../Button/button.component";
+
 const ProfileImgCard = () => {
+  const navigate = useNavigate();
+
+  const goToResumeHandler = () => {
+    navigate("resume");
+  };
+
   return (
     <Fragment>
       <Title>My name is William Lowrimore...</Title>
@@ -18,6 +29,11 @@ const ProfileImgCard = () => {
           <AboutText />
         </ImageCardContainer>
       </AboutContainer>
+      <ButtonContainer>
+        <Button buttonType={"base"} onClick={goToResumeHandler}>
+          Jump to Resume
+        </Button>
+      </ButtonContainer>
     </Fragment>
   );
 };
--- portfolio-code/src/components/profile/profile-image-card.styles.jsx
+++ portfolio-code/src/components/profile/profile-image-card.styles.jsx
@@ -30,3 +30,11 @@ export const ImageCardContainer = styled.div`
     border-radius: 8px;
   }
 `;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  width: 70%;
+  margin: auto;
+  padding: 14px 0;
+`;
